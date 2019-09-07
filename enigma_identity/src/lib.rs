1d9931112fffe78e42e7ae2261beac47d8fe2415
Val <valmack@protonmail.com>
2019-09-07 16:00:19 -0400

    Add identity storage struct

--- enigma_identity/src/lib.rs
+++ enigma_identity/src/lib.rs
@@ -1,5 +1,30 @@
+extern crate eng_wasm;
+extern crate eng_wasm_derive;
+use eng_wasm::*;
+use eng_wasm_derive::*;
+
 pub struct PrivateIdentity {
     address: String,
     age: u64,
     // TODO: Add more fields here and allow fields to be mutable.
+}
+
+pub struct IdentityStorage {
+   identities: Vec<PrivateIdentity>,
+}
+
+impl IdentityStorage {
+    pub fn new() -> IdentityStorage {
+        IdentityStorage {
+            identities: Vec::new(),
+        }
+    }
+
+    pub fn add_identity(&mut self, address: String, age: u64) {
+        let identity = PrivateIdentity {
+            address,
+            age,
+        };
+        self.identities.push(identity);
+    }
 }
--- enigma_identity/src/main.rs
+++ enigma_identity/src/main.rs
@@ -1,3 +1,8 @@
+extern crate enigma_identity;
+use enigma_identity::IdentityStorage;
+
 fn main() {
-    println!("Hello, world!");
+    let mut identity_storage = IdentityStorage::new();
+    let address = String::from("eth_address");
+    identity_storage.add_identity(address, 25);
 }
