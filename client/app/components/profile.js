92fb06c16b08b4e223af4e5cdc0bed76ac184f2d
Jen Weber <j@jenweber.me>
2019-09-07 16:45:17 -0400

    create mock badge

--- client/app/components/profile.js
+++ client/app/components/profile.js
@@ -1,6 +1,22 @@
 import Component from '@ember/component';
+import { computed } from '@ember/object';
 
 export default Component.extend({
   size: '',
   isFittedShirt: false,
+  isVegan: false,
+  isVegetarian: false,
+  isGlutenFree: false,
+  otherDietary: '',
+  firstName: '',
+  lastName: '',
+  identity: 'http://example.com',
+  nickname: computed('firstName', 'lastName', function() {
+    if (!this.firstName || !this.lastName) {
+      return ''
+    } else {
+      let lastInitial = this.lastName.slice(0, 1)
+      return `${this.firstName} ${lastInitial}.`
+    }
+  })
 });
