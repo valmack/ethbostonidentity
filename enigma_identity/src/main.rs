e5502b08d2d46f7b07d892f5072f0f3a4bb4d0e4
Val <valmack@protonmail.com>
2019-09-07 16:59:58 -0400

    Demo age requirement method

--- enigma_identity/src/main.rs
+++ enigma_identity/src/main.rs
@@ -4,5 +4,7 @@ use enigma_identity::IdentityStorage;
 fn main() {
     let mut identity_storage = IdentityStorage::new();
     let address = String::from("eth_address");
-    identity_storage.add_identity(address, 25);
+    identity_storage.add_identity(String::from(&address), 25);
+    let meets_age_requirement = identity_storage.is_age_at_least(String::from(&address), 30);
+    println!("Does user '{}' meet {} requirement? {}", String::from(&address), "age", meets_age_requirement)
 }
