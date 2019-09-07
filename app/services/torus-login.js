65290d8d9b69e5f7cf60268ee59b40ff99af508f
Jen Weber <j@jenweber.me>
2019-09-07 12:53:52 -0400

    remove debugger, oops

--- app/services/torus-login.js
+++ app/services/torus-login.js
@@ -12,7 +12,6 @@ export default Service.extend({
 		const torus = new Torus();
 		await torus.init();
 		await torus.login(); // await torus.ethereum.enable()
-		debugger
 		const web3 = new Web3(torus.provider);
 		this.set('web3', web3)
 	}
